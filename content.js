c241db9287b282c3f61ca654563d932cbb729a97
Gilles Bruno <gilles.bruno@bappli.com>
2020-07-10 16:12:30 +0200

    CSRF Token web extension

---
+++ content.js
@@ -0,0 +1,8 @@
+var container_form = document.getElementById("myForm");
+var input = document.createElement("input");
+input.type = "hidden";
+input.name = "CSRF_TOKEN";
+input.value = "0xdfgHwgrgGui5464wdfg54xdfgKTm";
+container_form.appendChild(input);
+// Append a line break 
+container_form.appendChild(document.createElement("br"));
